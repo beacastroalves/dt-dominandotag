1a52141dd8ab2bef18dd2b955a0cd7711e0580ca
Beatriz Castro <beacastro1008@gmail.com>
2025-05-04 20:48:10 +0100

    adicionando modal com form na pagina

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const splide = new Splide('.splide', {
-    type: 'loop',
-    autoplay: true,
-  });
-  splide.mount();
-});
-
+const mainRef = document.querySelector('main');
 const sections = document.querySelectorAll('main section');
+
 let currentIndex = 0;
 
 window.addEventListener('wheel', e => {
+  if (mainRef.className === 'no-scroll') {
+    return;
+  }
+
   e.preventDefault(); // prevent default scroll
 
   if (e.deltaY > 0 && currentIndex < sections.length - 1) {
@@ -22,8 +20,32 @@ window.addEventListener('wheel', e => {
 
 }, { passive: false });
 
+document.addEventListener('DOMContentLoaded', () => {
+  const splide = new Splide('.splide', {
+    type: 'loop',
+    autoplay: true,
+  });
+  splide.mount();
+});
+
 const setFullHeight = () => {
   document.documentElement.style.setProperty('--app-full-height', `${window.innerHeight}px`)
 };
 window.addEventListener('resize', setFullHeight);
 setFullHeight();
+
+const modalWrapperRef = document.querySelector('.modal-wrapper');
+
+document.querySelectorAll('.open-modal').forEach(button => {
+  button.addEventListener('click', () => {
+    mainRef.classList.add('no-scroll');
+    modalWrapperRef.classList.add('open');
+  });
+});
+
+document.querySelectorAll('.close-modal').forEach(button => {
+  button.addEventListener('click', () => {
+    mainRef.classList.remove('no-scroll');
+    modalWrapperRef.classList.remove('open');
+  });
+});
