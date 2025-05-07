bd48fdaac34ed17812779a82872f2621f4891b39
Beatriz Castro <beacastro1008@gmail.com>
2025-05-07 14:42:27 +0100

    retirando true do video assistido

--- assets/js/vimeo.js
+++ assets/js/vimeo.js
@@ -21,8 +21,9 @@ vimeoIframeRef.setAttribute('allow', 'autoplay; fullscreen; encrypted-media');
 vimeoIframeRef.setAttribute('src', `https://player.vimeo.com/video/${vimeoIframeRef.getAttribute('vimeo')}?background=0&title=0&byline=0&portrait=0&controls=0&autoplay=1&muted=1`);
 vimeoFirstClickRef.classList.add('visible');
 
+const finished = localStorage.getItem('vimeo-video-finished');
+
 const player = new Vimeo.Player(vimeoIframeRef);
-localStorage.setItem('vimeo-video-finished', true);
 player.ready().then(() => {
   let isFirstClick = true;
 
