7f2d9b013bc3311f648690109ad9293cde0c1b60
Beatriz Castro <beacastro1008@gmail.com>
2025-05-10 15:53:07 +0100

    Implementa validações do formulário parte 1

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,7 +1,84 @@
+console.log('Form started!');
 const startForm = dataModal => {
-  console.log('Form started!');
-  const modalButtonRef = document.querySelector('#modal-button');
-  modalButtonRef.addEventListener('click', () => {
-    window.location.href = dataModal.url;
+  const formRef = document.querySelector('#form-vsl');
+  const controlRefs = formRef.querySelectorAll('input');
+  const buttonRef = formRef.querySelector('button');
+
+  const validateAllControls = () => {
+    let disableTheButton = false;
+    controlRefs.forEach(controlRef => {
+      let errorName = '';
+
+      switch (controlRef.name) {
+        case 'name':
+          if (!controlRef.value) {
+            errorName = 'required';
+          } else if (controlRef.value.length < 4) {
+            errorName = 'minLength';
+          }
+          break;
+        case 'email':
+          if (!controlRef.value) {
+            errorName = 'required';
+          }
+          break;
+        case 'whatsapp':
+          if (!controlRef.value) {
+            errorName = 'required';
+          }
+          break;
+      }
+
+      const smallRefs = controlRef.parentElement.querySelectorAll('small');
+      controlRef.classList.remove('invalid');
+      smallRefs.forEach(smallRef => {
+        smallRef.style.display = 'none';
+      });
+
+      if (errorName) {
+        controlRef.classList.add('invalid');
+        disableTheButton = true;
+
+        if (controlRef.classList.contains('touched')) {
+          smallRefs.forEach(smallRef => {
+            if (smallRef.classList.contains(errorName)) {
+              smallRef.style.display = 'block';
+            }
+          });
+        }
+      }
+    });
+
+    if (disableTheButton) {
+      buttonRef.setAttribute('disabled', true);
+    } else {
+      buttonRef.removeAttribute('disabled');
+    }
+  };
+
+  controlRefs.forEach(controlRef => {
+    controlRef.addEventListener('input', () => {
+      console.log(controlRef.value);
+      validateAllControls();
+    });
+
+    controlRef.addEventListener('blur', () => {
+      controlRef.classList.add('touched');
+      validateAllControls();
+    });
   });
+
+  buttonRef.addEventListener('click', () => {
+    if (buttonRef.getAttribute('disabled')) {
+      return;
+    }
+
+    const obj = {};
+    controlRefs.forEach(controlRef => obj[controlRef.name] = controlRef.value);
+
+    console.log(obj);
+    // window.location.href = dataModal.url;
+  });
+
+  validateAllControls();
 };
